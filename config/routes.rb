6061ca8f9c5b6513ba544346221c220db5b027f5
sclarke5 <sclarke5555@gmail.com>
2018-10-18 18:18:14 -0400

    basic but working

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resource :articles
+      resources :articles
     end
-  end 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
